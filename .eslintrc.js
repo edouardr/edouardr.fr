0ca0cd06bf013839bb55c36a7e2254d5681833d3
edouardr <riviere.edouard@gmail.com>
2019-12-29 23:35:20 +0100

    SETUP
    Add gatsby-starter + TypeScript support + Jest + Formatting with prettier + Linting

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  globals: {
+    __PATH_PREFIX__: true,
+  },
+  extends: [
+    `plugin:@typescript-eslint/eslint-recommended`,
+    `plugin:@typescript-eslint/recommended`,
+    `eslint-config-prettier/@typescript-eslint`,
+  ],
+  overrides: [
+    {
+      files: '**/*.+(ts|tsx)',
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: 'tsconfig.json',
+      },
+      plugins: ['@typescript-eslint/eslint-plugin'],
+    },
+  ],
+};
