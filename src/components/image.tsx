9a4f1b36d0ce50af7ef8f8933ba5bc12bd4a497f
edouardr <riviere.edouard@gmail.com>
2020-01-07 10:45:57 +0100

    Use WebP format for image component

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -7,8 +7,8 @@ const Image = (): JSX.Element => {
     query {
       placeholderImage: file(relativePath: { eq: "ed-working.jpeg" }) {
         childImageSharp {
-          fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
+          fluid(maxWidth: 300, quality: 80) {
+            ...GatsbyImageSharpFluid_withWebp
           }
         }
       }
